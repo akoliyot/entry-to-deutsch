72b0ea1e876c34ecc4cf78282d0ae977197c99b3
Aswin Koliyot <akoliyot@gmail.com>
2020-05-16 22:21:07 +0200

    Add TypeScript and remove unused components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Alphabet } from "./components/Alphabet/Alphabet";
 
 function App() {
   return (
     <div className="App">
+      <Alphabet />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Alphabet/Alphabet.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Alphabet: React.FC = () => {
+  return <h1>A</h1>;
+};
