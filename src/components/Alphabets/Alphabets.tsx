ffc80e8d0ec636f015c29fb4975b06e9b0c48dd9
Aswin Koliyot <akoliyot@gmail.com>
2020-05-21 19:22:11 +0200

    Add audios for core 26 alphabets

--- src/components/Alphabets/Alphabets.tsx
+++ src/components/Alphabets/Alphabets.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 import { Alphabet } from '../Alphabet/Alphabet';
 
 export const Alphabets: React.FC = () => {
-  return (
-    <>
-      <Alphabet char="a" />
-      <Alphabet char="b" />
-      <Alphabet char="c" />
-    </>
-  );
+  const generateAlphabets = () => {
+    const charCodeForLowerA = 97;
+    const charCodeForLowerZ = 122;
+    let charCode = charCodeForLowerA;
+
+    const alphabetComponents = [];
+
+    while (charCode <= charCodeForLowerZ) {
+      alphabetComponents.push(
+        <Alphabet char={String.fromCharCode(charCode)} />
+      );
+      charCode++;
+    }
+
+    return alphabetComponents;
+  };
+
+  return <>{generateAlphabets()}</>;
 };
