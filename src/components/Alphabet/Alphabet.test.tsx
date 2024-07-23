99fcbb2f908461d2e63edca1bdcc7569c850bb6d
Aswin Koliyot <akoliyot@gmail.com>
2024-07-24 00:49:11 +0200

    fix: test for Alphabet

--- src/components/Alphabet/Alphabet.test.tsx
+++ src/components/Alphabet/Alphabet.test.tsx
@@ -1,17 +1,21 @@
-import { Alphabet } from "./Alphabet"
+import { Alphabet } from './Alphabet';
 import React from 'react';
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react';
 
 describe('Alphabet', () => {
   it('should match snapshot', () => {
-    expect(<Alphabet isActive={false} char="a" />).toMatchSnapshot();
-  })
+    expect(
+      <Alphabet isActive={false} char="a" shortcutKey="a" />,
+    ).toMatchSnapshot();
+  });
 
   it('should render the correct character', () => {
-    const chosenChar = 'a'
-    const { container, debug } = render(<Alphabet isActive={false} char={chosenChar} />);
+    const chosenChar = 'a';
+    const { container, debug } = render(
+      <Alphabet isActive={false} char={chosenChar} shortcutKey={chosenChar} />,
+    );
     debug();
     const char = container.querySelector('p')!.innerHTML;
-    expect(char).toBe(chosenChar)
-  })
-})
+    expect(char).toBe(chosenChar);
+  });
+});
